3832e81d04ef9129d4302ff5c9d9b5195db5cc23
wanze <a358003542@outlook.com>
2023-10-01 01:48:13 +0800

    update

--- djangowander/settings.py
+++ djangowander/settings.py
@@ -85,17 +85,25 @@ WSGI_APPLICATION = 'djangowander.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangowander',
-        'USER': 'wander',
-        'PASSWORD': 'admin@123',
-        'HOST': '192.168.30.130',
-        'PORT': 5432
+import os
+if os.getenv('GITHUB') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'djangowander',
+            'USER': 'wander',
+            'PASSWORD': 'admin@123',
+            'HOST': '192.168.30.130',
+            'PORT': 5432
+        }
+    }
 
 
 # Password validation
